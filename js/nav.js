5b069a2d25cf4723f20f504d9ca5ac75939a830a
LukeStro47 <lukestrong47@gmail.com>
2019-12-18 19:30:04 -0500

    Signed-off-by: LukeStro47 <lukestrong47@gmail.com>

--- js/nav.js
+++ js/nav.js
@@ -1,17 +1,17 @@
 var disappear = document.getElementsByClassName("disapper");
 if(window.innerWidth < 601) {
-	for(var i = 0; i < 5; i++) {
+	for(var i = 0; i < 7; i++) {
 		disappear[i].style.display = "none";
 	}
 	document.getElementById('topnav').style.display = "block";
 }
 function myFunction() {
 	if(disappear[2].style.display == "block") {
-		for(var i = 0; i < 5; i++) {
+		for(var i = 0; i < 7; i++) {
 			disappear[i].style.display = "none";
 		}
 	} else {
-		for(var i = 1; i < 5; i++) {
+		for(var i = 1; i < 7; i++) {
 			disappear[i].style.display = "block";
 		}
 	}
