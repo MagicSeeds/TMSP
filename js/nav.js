b441b1c9d8f226a9d9d9fe451f058f90fbaca734
LukeStro47 <lukestrong47@gmail.com>
2020-02-15 16:31:05 -0500

    Signed-off-by: LukeStro47 <lukestrong47@gmail.com>

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,21 @@
+var modal = document.getElementById('modal');
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+    modal.style.display = "none";
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+TimeMe.initialize({
+    currentPageName: "home-page",
+    idleTimeoutInSeconds: 30
+});
+TimeMe.startTimer("home-page");
+TimeMe.callAfterTimeElapsedInSeconds(16, function(){
+	modal.style.display = "block";
+});
 var disappear = document.getElementsByClassName("disapper");
 if(window.innerWidth < 601) {
 	for(var i = 0; i < 7; i++) {
